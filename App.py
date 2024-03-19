f6175bd5484c84b10c436daba9a7a4ad2effff19
AngusCrowe <crowea@pedarecc.sa.edu.au>
2024-03-19 11:36:33 +1030

    trying to find temp values

--- App.py
+++ App.py
@@ -9,10 +9,10 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/')
 def index():
 
-    x_time = [0, 1, 2, 3, 4, 5, 6, 7]
-    y_temp = [25, 30, 32, 28, 21, 19, 18, 17]
-    plt.plot(x_time, y_temp)
-    plt.savefig("test_chart")
+    # x_time = [0, 1, 2, 3, 4, 5, 6, 7]
+    # y_temp = [25, 30, 32, 28, 21, 19, 18, 17]
+    # plt.plot(x_time, y_temp)
+    # plt.savefig("test_chart")
 
     return render_template('home.html')
 
@@ -37,20 +37,49 @@ def results():
     response2 = requests.get(url2).json()
 
     weatherinfo = response2.get("list")
-    datetime1 = response2.get("list")[0].get('dt')
-    print(datetime1)
-    datetime2 = response2.get("list")[1].get('dt')
-    print(datetime2)
-    datetime3 = response2.get("list")[2].get('dt')
-    print(datetime3)
-    datetime4 = response2.get("list")[3].get('dt')
-    print(datetime4)
-    datetime5 = response2.get("list")[4].get('dt')
-    print(datetime5)
-    datetime6 = response2.get("list")[5].get('dt')
-    print(datetime6)
-    datetime7 = response2.get("list")[6].get('dt')
-    print(datetime7)
+
+    main1 = response2.get("list")[1].get("main")
+    temp_value1 = main1['temp']
+    main2 = response2.get("list")[2].get("main")
+    temp_value2 = main2['temp']
+    main3 = response2.get("list")[3].get("main")
+    temp_value3 = main3['temp']
+    main4 = response2.get("list")[4].get("main")
+    temp_value4 = main4['temp']
+    main5 = response2.get("list")[5].get("main")
+    temp_value5 = main5['temp']
+    main6 = response2.get("list")[6].get("main")
+    temp_value6 = main6['temp']
+    main7 = response2.get("list")[7].get("main")
+    temp_value7 = main7['temp']
+    main8 = response2.get("list")[8].get("main")
+    temp_value8 = main8['temp']
+    main9 = response2.get("list")[9].get("main")
+    temp_value9 = main9['temp']
+
+
+    # datetime2 = response2.get("list")[1].get('dt')
+    # print(datetime2)
+    # datetime3 = response2.get("list")[2].get('dt')
+    # print(datetime3)
+    # datetime4 = response2.get("list")[3].get('dt')
+    # print(datetime4)
+    # datetime5 = response2.get("list")[4].get('dt')
+    # print(datetime5)
+    # datetime6 = response2.get("list")[5].get('dt')
+    # print(datetime6)
+    # datetime7 = response2.get("list")[6].get('dt')
+    # print(datetime7)
+    # datetime8 = response2.get("list")[7].get('dt')
+    # print(datetime8)
+    # datetime9 = response2.get("list")[8].get('dt')
+    # print(datetime9)
+
+    x_time2 = [0, 3, 6, 9, 12, 15, 18, 21]
+    y_temp2 = [temp_value1, temp_value2, temp_value3, temp_value4, temp_value5, temp_value6, temp_value7, temp_value8]
+
+    plt.plot(x_time2, y_temp2)
+    plt.savefig("weather_chart")
 
     weather_list = response.get("weather", [{}])
     weather_one = weather_list[0]
