5be20ce7685551292b2bd00a376279d71696bd07
AngusCrowe <crowea@pedarecc.sa.edu.au>
2024-03-22 14:56:35 +1030

    day and night temperature values were added below the icons for the coming week

--- App.py
+++ App.py
@@ -47,54 +47,66 @@ def results():
     response3 = requests.get(url3).json()
 
     icon01 = response3.get("daily")[0].get("weather")[0].get("icon")
+    k_max_temp0 = response3.get("daily")[0].get("temp").get("max")
+    k_min_temp0 = response3.get("daily")[0].get("temp").get("min")
+
     icon11 = response3.get("daily")[1].get("weather")[0].get("icon")
+    k_max_temp1 = response3.get("daily")[1].get("temp").get("max")
+    k_min_temp1 = response3.get("daily")[1].get("temp").get("min")
+
     icon21 = response3.get("daily")[2].get("weather")[0].get("icon")
+    k_max_temp2 = response3.get("daily")[2].get("temp").get("day")
+    k_min_temp2 = response3.get("daily")[2].get("temp").get("night")
+
     icon31 = response3.get("daily")[3].get("weather")[0].get("icon")
+    k_max_temp3 = response3.get("daily")[3].get("temp").get("day")
+    k_min_temp3 = response3.get("daily")[3].get("temp").get("night")
+
     icon41 = response3.get("daily")[4].get("weather")[0].get("icon")
+    k_max_temp4 = response3.get("daily")[4].get("temp").get("day")
+    k_min_temp4 = response3.get("daily")[4].get("temp").get("night")
+
     icon51 = response3.get("daily")[5].get("weather")[0].get("icon")
+    k_max_temp5 = response3.get("daily")[5].get("temp").get("day")
+    k_min_temp5 = response3.get("daily")[5].get("temp").get("night")
+
     icon61 = response3.get("daily")[6].get("weather")[0].get("icon")
+    k_max_temp6 = response3.get("daily")[6].get("temp").get("day")
+    k_min_temp6 = response3.get("daily")[6].get("temp").get("night")
 
-    main0 = response2.get("list")[0].get("main")
-    temp_value0 = main0['temp']
-    datetime0 = response2.get("list")[0].get('dt_txt')
-
-    main1 = response2.get("list")[1].get("main")
-    temp_value1 = main1['temp']
-    datetime1 = response2.get("list")[1].get('dt_txt')
-
-    main2 = response2.get("list")[2].get("main")
-    temp_value2 = main2['temp']
-    datetime2 = response2.get("list")[2].get('dt_txt')
-
-    main3 = response2.get("list")[3].get("main")
-    temp_value3 = main3['temp']
-    datetime3 = response2.get("list")[3].get('dt_txt')
-
-    main4 = response2.get("list")[4].get("main")
-    temp_value4 = main4['temp']
-    datetime4 = response2.get("list")[4].get('dt_txt')
-
-    main5 = response2.get("list")[5].get("main")
-    temp_value5 = main5['temp']
-    datetime5 = response2.get("list")[5].get('dt_txt')
-
-    temperatures = [temp_value0, temp_value1, temp_value2, temp_value3, temp_value4, temp_value5]
-    datetimes = [datetime0, datetime1, datetime2, datetime3, datetime4, datetime5]
+    # main0 = response2.get("list")[0].get("main")
+    # temp_value0 = main0['temp']
+    # datetime0 = response2.get("list")[0].get('dt_txt')
+    #
+    # main1 = response2.get("list")[1].get("main")
+    # temp_value1 = main1['temp']
+    # datetime1 = response2.get("list")[1].get('dt_txt')
+    #
+    # main2 = response2.get("list")[2].get("main")
+    # temp_value2 = main2['temp']
+    # datetime2 = response2.get("list")[2].get('dt_txt')
+    #
+    # main3 = response2.get("list")[3].get("main")
+    # temp_value3 = main3['temp']
+    # datetime3 = response2.get("list")[3].get('dt_txt')
+    #
+    # main4 = response2.get("list")[4].get("main")
+    # temp_value4 = main4['temp']
+    # datetime4 = response2.get("list")[4].get('dt_txt')
+    #
+    # main5 = response2.get("list")[5].get("main")
+    # temp_value5 = main5['temp']
+    # datetime5 = response2.get("list")[5].get('dt_txt')
+    #
+    # temperatures = [temp_value0, temp_value1, temp_value2, temp_value3, temp_value4, temp_value5]
+    # datetimes = [datetime0, datetime1, datetime2, datetime3, datetime4, datetime5]
 
     weather_list = response.get("weather", [{}])
     weather_one = weather_list[0]
     location = response.get("name")
-    timezone = response.get("timezone")
     timestamp = response.get("dt")
-    timestamp_local = datetime.fromtimestamp(timestamp)
     description = weather_one.get("description")
     temp_k = response.get("main", {}).get("temp")
-    if measurement == "celsius":
-        rounded_temp = round(int(temp_k) - 273.15)
-        temp = str(rounded_temp) + "°C"
-    elif measurement == "fahrenheit":
-        rounded_temp = round((int(temp_k) - 273.15) * 1.8 + 32)
-        temp = str(rounded_temp) + "°F"
     wind_speed_metres = response.get("wind", {}).get("speed")
     wind_speed = round((int(wind_speed_metres) * 3.6))
     icon = weather_one.get("icon")
@@ -107,6 +119,61 @@ def results():
     icon5 = "https://openweathermap.org/img/wn/" + icon51 + "@2x.png"
     icon6 = "https://openweathermap.org/img/wn/" + icon61 + "@2x.png"
     humidity = response.get("main", {}).get("humidity")
+
+    if measurement == "celsius":
+        rounded_temp = round(int(temp_k) - 273.15)
+        temp = str(rounded_temp) + "°C"
+        min0 = round(int(k_min_temp0) - 273.15)
+        min1 = round(int(k_min_temp1) - 273.15)
+        min2 = round(int(k_min_temp2) - 273.15)
+        min3 = round(int(k_min_temp3) - 273.15)
+        min4 = round(int(k_min_temp4) - 273.15)
+        min5 = round(int(k_min_temp5) - 273.15)
+        min6 = round(int(k_min_temp6) - 273.15)
+        max0 = round(int(k_max_temp0) - 273.15)
+        max1 = round(int(k_max_temp1) - 273.15)
+        max2 = round(int(k_max_temp2) - 273.15)
+        max3 = round(int(k_max_temp3) - 273.15)
+        max4 = round(int(k_max_temp4) - 273.15)
+        max5 = round(int(k_max_temp5) - 273.15)
+        max6 = round(int(k_max_temp6) - 273.15)
+
+    elif measurement == "fahrenheit":
+        rounded_temp = round((int(temp_k) - 273.15) * 1.8 + 32)
+        temp = str(rounded_temp) + "°F"
+        min0 = round((int(k_min_temp0) - 273.15) * 1.8 + 32)
+        min1 = round((int(k_min_temp1) - 273.15) * 1.8 + 32)
+        min2 = round((int(k_min_temp2) - 273.15) * 1.8 + 32)
+        min3 = round((int(k_min_temp3) - 273.15) * 1.8 + 32)
+        min4 = round((int(k_min_temp4) - 273.15) * 1.8 + 32)
+        min5 = round((int(k_min_temp5) - 273.15) * 1.8 + 32)
+        min6 = round((int(k_min_temp6) - 273.15) * 1.8 + 32)
+        max0 = round((int(k_max_temp0) - 273.15) * 1.8 + 32)
+        max1 = round((int(k_max_temp1) - 273.15) * 1.8 + 32)
+        max2 = round((int(k_max_temp2) - 273.15) * 1.8 + 32)
+        max3 = round((int(k_max_temp3) - 273.15) * 1.8 + 32)
+        max4 = round((int(k_max_temp4) - 273.15) * 1.8 + 32)
+        max5 = round((int(k_max_temp5) - 273.15) * 1.8 + 32)
+        max6 = round((int(k_max_temp6) - 273.15) * 1.8 + 32)
+
+    temps_dict = {
+        "min0": min0,
+        "min1": min1,
+        "min2": min2,
+        "min3": min3,
+        "min4": min4,
+        "min5": min5,
+        "min6": min6,
+        "max0": max0,
+        "max1": max1,
+        "max2": max2,
+        "max3": max3,
+        "max4": max4,
+        "max5": max5,
+        "max6": max6,
+    }
+
+    print(temps_dict)
 
     icons_dict = {
         "icon0": icon0,
@@ -127,7 +194,7 @@ def results():
         "humidity": humidity
     }
 
-    return render_template('weatherdata.html', weather_dict=weather_dict, response=response, icons_dict=icons_dict)
+    return render_template('weatherdata.html', weather_dict=weather_dict, response=response, icons_dict=icons_dict, temps_dict=temps_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
